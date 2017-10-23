c42aa374577c06d1764e12a1cd5d5568b1a87fc7
Bart³omiej Kordek <bartekordek@gmail.com>
2017-10-23 23:53:27 +0200

    Changed tests.

--- TestApp/main.cpp
+++ TestApp/main.cpp
@@ -5,22 +5,24 @@
 int main( int argc, char** argv )
 {
 	auto sdlW = SDL2W::getSDL2Wrapper();
-	auto window = sdlW->createWindow( CUL::XYPosition<int>( 500, 500 ), CUL::Size<unsigned>( 800, 600 ), "Windows 1" );
+	auto window = sdlW->createWindow( CUL::XYPosition<int>( 1922, 64 ), CUL::Size<unsigned>( 800, 600 ), "Windows 1" );
 	
 	auto obj = window->createObject( CUL::FS::Path( "../../Media/pikaczu.bmp") );
+	obj->setScale( CUL::Math::Vector3Dd( 0.0, 0.0, 0.0 ) );
 
-	unsigned ticks = 128;
+	unsigned ticks = 4096;
 	double timeInSeconds = 60;
 	auto tickTimeInMs = static_cast<unsigned>( 1000 * timeInSeconds / ticks );
 	for( unsigned int i = 0; i < ticks; ++i )
 	{
+		sdlW->clearWindows();
 		sdlW->renderFrame();
 		sdlW->refreshScreen();
 		std::this_thread::sleep_for( 
 			std::chrono::milliseconds( 
 				tickTimeInMs ) );
 		obj->setScale( 
-			CUL::Math::Vector3Du( i / ticks, i / ticks, 0 ) );
+			CUL::Math::Vector3Dd( 4.0 * i / ticks, 4.0 * i / ticks, 0 ) );
 	}
 
 	return 0;
