17662ddb34980ceda92fd16f9a7b2fc4b7abafa7
Bartłomiej Kordek <bartekordek@gmail.com>
2017-11-28 14:41:27 +0100

    Removed SDL repo.

--- Deps/SDL2/SDL2-2.0.5/Build-Linux/SetHome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-CURRENT_DIR=`pwd`
-LIB_NAME=SDL2_LINUX
-KEY_NAME=HOME_$LIB_NAME
-export $KEY_NAME=$CURRENT_DIR
-setx $KEY_NAME $CURRENT_DIR 
-echo "export $KEY_NAME=$CURRENT_DIR" >> ~/.bashrc
