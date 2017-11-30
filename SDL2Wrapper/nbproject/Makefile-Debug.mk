ed56b56f991676f23a747fcef093e28bef929f17
Bartłomiej Kordek <bartekordek@gmail.com>
2017-11-30 10:04:06 +0100

    Added net beans proj.

---
+++ SDL2Wrapper/nbproject/Makefile-Debug.mk
@@ -0,0 +1,127 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include SDL2Wrapper-Makefile.mk
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/src/IObject.o \
+	${OBJECTDIR}/src/ISDL2Wrapper.o \
+	${OBJECTDIR}/src/IWindow.o \
+	${OBJECTDIR}/src/RegularSDL2Window.o \
+	${OBJECTDIR}/src/SDL2Wrapper.o \
+	${OBJECTDIR}/src/SDL2WrapperImpl.o \
+	${OBJECTDIR}/src/Sprite.o \
+	${OBJECTDIR}/src/TextureWrapper.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=../deps/CUL/build/Cygwin-Windows-Debug/libcul.a
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libSDL2Wrapper.${CND_DLIB_EXT}
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libSDL2Wrapper.${CND_DLIB_EXT}: ../deps/CUL/build/Cygwin-Windows-Debug/libcul.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libSDL2Wrapper.${CND_DLIB_EXT}: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libSDL2Wrapper.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared
+
+${OBJECTDIR}/src/IObject.o: src/IObject.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/IObject.o src/IObject.cpp
+
+${OBJECTDIR}/src/ISDL2Wrapper.o: src/ISDL2Wrapper.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/ISDL2Wrapper.o src/ISDL2Wrapper.cpp
+
+${OBJECTDIR}/src/IWindow.o: src/IWindow.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/IWindow.o src/IWindow.cpp
+
+${OBJECTDIR}/src/RegularSDL2Window.o: src/RegularSDL2Window.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/RegularSDL2Window.o src/RegularSDL2Window.cpp
+
+${OBJECTDIR}/src/SDL2Wrapper.o: src/SDL2Wrapper.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/SDL2Wrapper.o src/SDL2Wrapper.cpp
+
+${OBJECTDIR}/src/SDL2WrapperImpl.o: src/SDL2WrapperImpl.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/SDL2WrapperImpl.o src/SDL2WrapperImpl.cpp
+
+${OBJECTDIR}/src/Sprite.o: src/Sprite.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Sprite.o src/Sprite.cpp
+
+${OBJECTDIR}/src/TextureWrapper.o: src/TextureWrapper.cpp nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Werror -DSDL2Wrapper_DYNAMIC -Iinc -I../deps/CUL -I../deps/SDL2-2.0.7/include -I../deps/boost_1_64_0 -std=c++14  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/TextureWrapper.o src/TextureWrapper.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
