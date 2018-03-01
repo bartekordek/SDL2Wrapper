761a23d233f1e223ad74c36fbfefd321b2e9a7c4
Bartłomiej Kordek <bartekordek@gmail.com>
2018-03-01 22:31:31 +0100

    Fixed building.

--- libsdl2wrapper/inc/SDL2Wrapper/SDL2Wrapper.hpp
+++ libsdl2wrapper/inc/SDL2Wrapper/SDL2Wrapper.hpp
@@ -1,5 +1,17 @@
 #pragma once
-/*
+
+#if defined SDL2W_EXPORT && defined SDL2W_LINUX
+#define SDL2WrapperAPI
+#define SDL2Wrapper_TEMPLATE
+#elif SDL2W_EXPORT
+#define SDL2WrapperAPI __declspec(dllexport)
+#define SDL2Wrapper_TEMPLATE
+#else
+#define SDL2WrapperAPI __declspec(dllimport)
+#define SDL2Wrapper_TEMPLATE extern
+#endif
+/*TODO: Find a way to tell library name.
+
 #ifdef _MSC_VER && SDL2Wrapper_DYNAMIC
 #pragma comment(lib, "SDL2WrapperDynamic.lib")
 #elif  MSC_VER
@@ -8,11 +20,3 @@
 #pragma comment(lib, "libsdl2wrapper.a")
 #endif
 */
-
-#ifdef SDL2Wrapper_DYNAMIC
-#define SDL2WrapperAPI __declspec(dllexport)
-#define SDL2Wrapper_TEMPLATE
-#else
-#define SDL2WrapperAPI __declspec(dllimport)
-#define SDL2Wrapper_TEMPLATE extern
-#endif
