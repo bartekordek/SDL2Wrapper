309507b7e973a16a68b1dbbd514406b14de7fe25
Bartłomiej Kordek <bartekordek@gmail.com>
2021-03-07 02:20:50 +0100

    Build fixes.

--- cmake_modules/Findsdl2.cmake
+++ cmake_modules/Findsdl2.cmake
@@ -1,6 +1,6 @@
 set( sdl2_repo_name sdl2-repo )
 set( sdl2_root CMAKE_CURRENT_SOURCE_DIR/../deps/${sdl2_repo_name} )
 set( sdl2_include ${sdl2_ROOT}/include )
-set( sdl2_binary_dir ${CMAKE_BINARY_DIR}/deps/${sdl2_repo_name}/${CMAKE_BUILD_TYPE} )
+set( sdl2_binary_dir ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE} )
 message( "[Set sdl2_binary_dir to ${sdl2_binary_dir}]" )
 set( sdl2_found true )
